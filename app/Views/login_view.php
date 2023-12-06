5fcd5d268c4b714a85aa09517b64b5f55fa4c21b
Jeferson Oliveita Forte <jefersonof@hotmail.com>
2023-12-06 13:58:40 -0300

    Substituindo o front-end do Login.
    Substituindo o front-end do Login.

--- app/Views/login_view.php
+++ app/Views/login_view.php
@@ -16,14 +16,14 @@ License: You must have a valid license purchased only from themeforest(the above
 	<!--begin::Head-->
 	<head>
 		<meta charset="utf-8" />
-		<title>Metronic | Sign In</title>
-		<meta name="description" content="Singin page example" />
+		<title>Metronic | Login Page 1</title>
+		<meta name="description" content="Login page example" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		<!--begin::Fonts-->
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
 		<!--end::Fonts-->
 		<!--begin::Page Custom Styles(used by this page)-->
-		<link href="assets/css/pages/login/login-3.css?v=7.0.5" rel="stylesheet" type="text/css" />
+		<link href="assets/css/pages/login/login-1.css?v=7.0.5" rel="stylesheet" type="text/css" />
 		<!--end::Page Custom Styles-->
 		<!--begin::Global Theme Styles(used by all pages)-->
 		<link href="assets/plugins/global/plugins.bundle.css?v=7.0.5" rel="stylesheet" type="text/css" />
@@ -44,87 +44,154 @@ License: You must have a valid license purchased only from themeforest(the above
 		<!--begin::Main-->
 		<div class="d-flex flex-column flex-root">
 			<!--begin::Login-->
-			<div class="login login-3 wizard d-flex flex-column flex-lg-row flex-column-fluid">
+			<div class="login login-1 login-signin-on d-flex flex-column flex-lg-row flex-column-fluid bg-white" id="kt_login">
 				<!--begin::Aside-->
-				<div class="login-aside d-flex flex-column flex-row-auto">
+				<div class="login-aside d-flex flex-column flex-row-auto" style="background-color: #F2C98A;">
 					<!--begin::Aside Top-->
 					<div class="d-flex flex-column-auto flex-column pt-lg-40 pt-15">
 						<!--begin::Aside header-->
-						<a href="#" class="login-logo text-center pt-lg-25 pb-10">
-							<img src="assets/media/logos/logo-1.png" class="max-h-70px" alt="" />
+						<a href="#" class="text-center mb-10">
+							<img src="assets/media/logos/logo-letter-1.png" class="max-h-70px" alt="" />
 						</a>
 						<!--end::Aside header-->
-						<!--begin::Aside Title-->
-						<h3 class="font-weight-bolder text-center font-size-h4 text-dark-50 line-height-xl">User Experience &amp; Interface Design
-						<br />Strategy SaaS Solutions</h3>
-						<!--end::Aside Title-->
+						<!--begin::Aside title-->
+						<h3 class="font-weight-bolder text-center font-size-h4 font-size-h1-lg" style="color: #986923;">Descubra o incrível Metronic 
+						<br />com ótimas ferramentas de construção</h3>
+						<!--end::Aside title-->
 					</div>
 					<!--end::Aside Top-->
 					<!--begin::Aside Bottom-->
-					<div class="aside-img d-flex flex-row-fluid bgi-no-repeat bgi-position-x-center" style="background-position-y: calc(100% + 5rem); background-image: url(assets/media/svg/illustrations/login-visual-5.svg)"></div>
+					<div class="aside-img d-flex flex-row-fluid bgi-no-repeat bgi-position-y-bottom bgi-position-x-center" style="background-image: url(assets/media/svg/illustrations/login-visual-1.svg)"></div>
 					<!--end::Aside Bottom-->
 				</div>
 				<!--begin::Aside-->
 				<!--begin::Content-->
-				<div class="login-content flex-row-fluid d-flex flex-column p-10">
-					<!--begin::Top-->
-					<div class="text-right d-flex justify-content-center">
-						<div class="top-signin text-right d-flex justify-content-end pt-5 pb-lg-0 pb-10">
-							<span class="font-weight-bold text-muted font-size-h4">Having issues?</span>
-							<a href="javascript:;" class="font-weight-bold text-primary font-size-h4 ml-2" id="kt_login_signup">Get Help</a>
-						</div>
-					</div>
-					<!--end::Top-->
-					<!--begin::Wrapper-->
-					<div class="d-flex flex-row-fluid flex-center">
+				<div class="login-content flex-row-fluid d-flex flex-column justify-content-center position-relative overflow-hidden p-7 mx-auto">
+					<!--begin::Content body-->
+					<div class="d-flex flex-column-fluid flex-center">
 						<!--begin::Signin-->
-						<div class="login-form">
+						<div class="login-form login-signin">
 							<!--begin::Form-->
-							<form class="form" id="kt_login_singin_form" action="">
+							<form class="form" novalidate="novalidate" id="kt_login_signin_form">
 								<!--begin::Title-->
-								<div class="pb-5 pb-lg-15">
-									<h3 class="font-weight-bolder text-dark font-size-h2 font-size-h1-lg">Sign In</h3>
-									<div class="text-muted font-weight-bold font-size-h4">New Here?
-									<a href="custom/pages/login/login-3/signup.html" class="text-primary font-weight-bolder">Create Account</a></div>
+								<div class="pb-13 pt-lg-0 pt-5">
+									<h3 class="font-weight-bolder text-dark font-size-h4 font-size-h1-lg">Seja bem vindo</h3>
+									<!--<span class="text-muted font-weight-bold font-size-h4">New Here?
+									<a href="javascript:;" id="kt_login_signup" class="text-primary font-weight-bolder">Create an Account</a></span>-->
 								</div>
 								<!--begin::Title-->
 								<!--begin::Form group-->
 								<div class="form-group">
-									<label class="font-size-h6 font-weight-bolder text-dark">Your Email</label>
-									<input class="form-control h-auto py-7 px-6 rounded-lg border-0" type="text" name="username" autocomplete="off" />
+									<label class="font-size-h6 font-weight-bolder text-dark">Login</label>
+									<input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg" type="text" name="username" autocomplete="off" />
 								</div>
 								<!--end::Form group-->
 								<!--begin::Form group-->
 								<div class="form-group">
 									<div class="d-flex justify-content-between mt-n5">
-										<label class="font-size-h6 font-weight-bolder text-dark pt-5">Your Name</label>
-										<a href="custom/pages/login/login-3/forgot.html" class="text-primary font-size-h6 font-weight-bolder text-hover-primary pt-5">Forgot Password ?</a>
+										<label class="font-size-h6 font-weight-bolder text-dark pt-5">Senha</label>
+										<!--<a href="javascript:;" class="text-primary font-size-h6 font-weight-bolder text-hover-primary pt-5" id="kt_login_forgot">Forgot Password ?</a>-->
 									</div>
-									<input class="form-control h-auto py-7 px-6 rounded-lg border-0" type="password" name="password" autocomplete="off" />
+									<input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg" type="password" name="password" autocomplete="off" />
 								</div>
 								<!--end::Form group-->
 								<!--begin::Action-->
 								<div class="pb-lg-0 pb-5">
-									<button type="submit" id="kt_login_singin_form_submit_button" class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mr-3">Sign In</button>
+									<!--<button type="button" id="kt_login_signin_submit" class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mr-3">Sign Inoo</button>-->
 									<button type="button" class="btn btn-light-primary font-weight-bolder px-8 py-4 my-3 font-size-lg">
 									<span class="svg-icon svg-icon-md">
 										<!--begin::Svg Icon | path:assets/media/svg/social-icons/google.svg-->
-										<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-											<path d="M19.9895 10.1871C19.9895 9.36767 19.9214 8.76973 19.7742 8.14966H10.1992V11.848H15.8195C15.7062 12.7671 15.0943 14.1512 13.7346 15.0813L13.7155 15.2051L16.7429 17.4969L16.9527 17.5174C18.879 15.7789 19.9895 13.221 19.9895 10.1871Z" fill="#4285F4" />
-											<path d="M10.1993 19.9313C12.9527 19.9313 15.2643 19.0454 16.9527 17.5174L13.7346 15.0813C12.8734 15.6682 11.7176 16.0779 10.1993 16.0779C7.50243 16.0779 5.21352 14.3395 4.39759 11.9366L4.27799 11.9466L1.13003 14.3273L1.08887 14.4391C2.76588 17.6945 6.21061 19.9313 10.1993 19.9313Z" fill="#34A853" />
-											<path d="M4.39748 11.9366C4.18219 11.3166 4.05759 10.6521 4.05759 9.96565C4.05759 9.27909 4.18219 8.61473 4.38615 7.99466L4.38045 7.8626L1.19304 5.44366L1.08875 5.49214C0.397576 6.84305 0.000976562 8.36008 0.000976562 9.96565C0.000976562 11.5712 0.397576 13.0882 1.08875 14.4391L4.39748 11.9366Z" fill="#FBBC05" />
-											<path d="M10.1993 3.85336C12.1142 3.85336 13.406 4.66168 14.1425 5.33717L17.0207 2.59107C15.253 0.985496 12.9527 0 10.1993 0C6.2106 0 2.76588 2.23672 1.08887 5.49214L4.38626 7.99466C5.21352 5.59183 7.50242 3.85336 10.1993 3.85336Z" fill="#EB4335" />
-										</svg>
+										<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"/></svg>
 										<!--end::Svg Icon-->
-									</span>Sign in with Google</button>
+									</span>Entrar</button>
 								</div>
 								<!--end::Action-->
 							</form>
 							<!--end::Form-->
 						</div>
 						<!--end::Signin-->
+						<!--begin::Signup-->
+						<div class="login-form login-signup">
+							<!--begin::Form-->
+							<form class="form" novalidate="novalidate" id="kt_login_signup_form">
+								<!--begin::Title-->
+								<div class="pb-13 pt-lg-0 pt-5">
+									<h3 class="font-weight-bolder text-dark font-size-h4 font-size-h1-lg">Sign Up</h3>
+									<p class="text-muted font-weight-bold font-size-h4">Enter your details to create your account</p>
+								</div>
+								<!--end::Title-->
+								<!--begin::Form group-->
+								<div class="form-group">
+									<input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6" type="text" placeholder="Fullname" name="fullname" autocomplete="off" />
+								</div>
+								<!--end::Form group-->
+								<!--begin::Form group-->
+								<div class="form-group">
+									<input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6" type="email" placeholder="Email" name="email" autocomplete="off" />
+								</div>
+								<!--end::Form group-->
+								<!--begin::Form group-->
+								<div class="form-group">
+									<input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6" type="password" placeholder="Password" name="password" autocomplete="off" />
+								</div>
+								<!--end::Form group-->
+								<!--begin::Form group-->
+								<div class="form-group">
+									<input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6" type="password" placeholder="Confirm password" name="cpassword" autocomplete="off" />
+								</div>
+								<!--end::Form group-->
+								<!--begin::Form group-->
+								<div class="form-group">
+									<label class="checkbox mb-0">
+									<input type="checkbox" name="agree" />I Agree the
+									<a href="#">terms and conditions</a>.
+									<span></span></label>
+								</div>
+								<!--end::Form group-->
+								<!--begin::Form group-->
+								<div class="form-group d-flex flex-wrap pb-lg-0 pb-3">
+									<button type="button" id="kt_login_signup_submit" class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mr-4">Submit</button>
+									<button type="button" id="kt_login_signup_cancel" class="btn btn-light-primary font-weight-bolder font-size-h6 px-8 py-4 my-3">Cancel</button>
+								</div>
+								<!--end::Form group-->
+							</form>
+							<!--end::Form-->
+						</div>
+						<!--end::Signup-->
+						<!--begin::Forgot-->
+						<div class="login-form login-forgot">
+							<!--begin::Form-->
+							<form class="form" novalidate="novalidate" id="kt_login_forgot_form">
+								<!--begin::Title-->
+								<div class="pb-13 pt-lg-0 pt-5">
+									<h3 class="font-weight-bolder text-dark font-size-h4 font-size-h1-lg">Forgotten Password ?</h3>
+									<p class="text-muted font-weight-bold font-size-h4">Enter your email to reset your password</p>
+								</div>
+								<!--end::Title-->
+								<!--begin::Form group-->
+								<div class="form-group">
+									<input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6" type="email" placeholder="Email" name="email" autocomplete="off" />
+								</div>
+								<!--end::Form group-->
+								<!--begin::Form group-->
+								<div class="form-group d-flex flex-wrap pb-lg-0">
+									<button type="button" id="kt_login_forgot_submit" class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mr-4">Submit</button>
+									<button type="button" id="kt_login_forgot_cancel" class="btn btn-light-primary font-weight-bolder font-size-h6 px-8 py-4 my-3">Cancel222</button>
+								</div>
+								<!--end::Form group-->
+							</form>
+							<!--end::Form-->
+						</div>
+						<!--end::Forgot-->
 					</div>
-					<!--end::Wrapper-->
+					<!--end::Content body-->
+					<!--begin::Content footer-->
+					<div class="d-flex justify-content-lg-start justify-content-center align-items-end py-7 py-lg-0">
+						<!--<a href="#" class="text-primary font-weight-bolder font-size-h5">Terms</a>
+						<a href="#" class="text-primary ml-10 font-weight-bolder font-size-h5">Plans</a>
+						<a href="#" class="text-primary ml-10 font-weight-bolder font-size-h5">Contact Us</a>-->
+					</div>
+					<!--end::Content footer-->
 				</div>
 				<!--end::Content-->
 			</div>
@@ -136,12 +203,12 @@ License: You must have a valid license purchased only from themeforest(the above
 		<script>var KTAppSettings = { "breakpoints": { "sm": 576, "md": 768, "lg": 992, "xl": 1200, "xxl": 1400 }, "colors": { "theme": { "base": { "white": "#ffffff", "primary": "#3699FF", "secondary": "#E5EAEE", "success": "#1BC5BD", "info": "#8950FC", "warning": "#FFA800", "danger": "#F64E60", "light": "#E4E6EF", "dark": "#181C32" }, "light": { "white": "#ffffff", "primary": "#E1F0FF", "secondary": "#EBEDF3", "success": "#C9F7F5", "info": "#EEE5FF", "warning": "#FFF4DE", "danger": "#FFE2E5", "light": "#F3F6F9", "dark": "#D6D6E0" }, "inverse": { "white": "#ffffff", "primary": "#ffffff", "secondary": "#3F4254", "success": "#ffffff", "info": "#ffffff", "warning": "#ffffff", "danger": "#ffffff", "light": "#464E5F", "dark": "#ffffff" } }, "gray": { "gray-100": "#F3F6F9", "gray-200": "#EBEDF3", "gray-300": "#E4E6EF", "gray-400": "#D1D3E0", "gray-500": "#B5B5C3", "gray-600": "#7E8299", "gray-700": "#5E6278", "gray-800": "#3F4254", "gray-900": "#181C32" } }, "font-family": "Poppins" };</script>
 		<!--end::Global Config-->
 		<!--begin::Global Theme Bundle(used by all pages)-->
-		<script src="../../public/assets/plugins/global/plugins.bundle.js?v=7.0.5"></script>
-		<script src="../../public/assets/plugins/custom/prismjs/prismjs.bundle.js?v=7.0.5"></script>
-		<script src="../../public/assets/js/scripts.bundle.js?v=7.0.5"></script>
+		<script src="assets/plugins/global/plugins.bundle.js?v=7.0.5"></script>
+		<script src="assets/plugins/custom/prismjs/prismjs.bundle.js?v=7.0.5"></script>
+		<script src="assets/js/scripts.bundle.js?v=7.0.5"></script>
 		<!--end::Global Theme Bundle-->
 		<!--begin::Page Scripts(used by this page)-->
-		<script src="../../public/assets/js/pages/custom/login/login-3.js?v=7.0.5"></script>
+		<script src="assets/js/pages/custom/login/login-general.js?v=7.0.5"></script>
 		<!--end::Page Scripts-->
 	</body>
 	<!--end::Body-->
